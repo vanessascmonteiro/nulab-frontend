7821786196b05bab9662ebc20858bc27ca213cfd
Michael Nascimento <michael.nsc@outlook.com>
2023-06-24 19:17:16 -0300

    aula 1 finalizada

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./app";
+
+import "./normalize.css";
+import "./colors.css";
+import "./defaults.css";
+
+const root = ReactDOM.createRoot(document.getElementById("raiz"));
+root.render(<App />);
