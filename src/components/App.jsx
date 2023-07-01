441cf7172dcf139d1dac32738073a03f33202772
Michael Nascimento <michael.nsc@outlook.com>
2023-07-01 19:27:55 -0300

    aula 02 - o inicio do começo

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+
+function App() {
+  // POSSO RETORNAR:
+  // strings
+  // arrays
+  // outros componentes
+  // jsx
+  // null ou undefined
+  // boolean
+  // numbers
+
+  // NÃO POSSO RETORNAR
+  // objetos
+
+  return <Header />;
+}
+
+export default App;
